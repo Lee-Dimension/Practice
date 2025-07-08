93c75df6b62128c23a0195b97f92850cc7e8d563
이차원 <ckdnjs7456@naver.com>
2025-07-09 00:51:17 +0900

    지피티 없이 혼자하기 1일차

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { StyleSheet, useColorScheme, View,} from 'react-native';
 import MainActivity from './MainActivity/MainActivity'
 
 function App() {
--- MainActivity/BarCodeButton.js
+++ MainActivity/BarCodeButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text,StyleSheet,View,Button,TouchableOpacity} from 'react-native';
+import {Text,StyleSheet,TouchableOpacity} from 'react-native';
 
 
 function BarCodeButton(){
