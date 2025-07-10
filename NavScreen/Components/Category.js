e7595fae1abaf475a51138639549aab8c07e3a6e
이차원 <ckdnjs4561@gmail.com>
2025-07-10 17:35:32 +0900

    7.9

--- NavScreen/Components/Category.js
+++ NavScreen/Components/Category.js
@@ -7,7 +7,6 @@ export default function Category(){
 
     const addCategory = () => {
         setCategories([...categories, {}]);
-
     };
 
     return(
--- NavScreen/Components/Distribution.js
+++ NavScreen/Components/Distribution.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View,StyleSheet,Text} from 'react-native';
 
-export default function Categorie(){
+export default function Distribution(){
 
     return(
         <Text>안녕</Text>
